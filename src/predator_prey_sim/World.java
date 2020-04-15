c6d7e143f38b7179fd227c0a784a2d6c3ae35687
Kayleigh Calder <59836279+calderkayleigh@users.noreply.github.com>
2020-04-14 22:24:13 -0400

    Update World.java

--- src/predator_prey_sim/World.java
+++ src/predator_prey_sim/World.java
@@ -6,15 +6,15 @@ import java.awt.*;
 import java.util.ArrayList;
 
 
-public class World {
+public class World
+{
 
-	private int width, height;
-	private Color canavasColor;
-	
+	public int width, height;
+	public static Color canvasColor;
+
 	//create array lists of predators and prey
-	ArrayList<Predator> predators = new ArrayList();
-	ArrayList<Prey> prey = new ArrayList();
-	
+	static ArrayList<Predator> predators = new ArrayList();
+	static ArrayList<Prey> prey = new ArrayList();
 
 	/**
 	 * @param w width of city
@@ -22,61 +22,72 @@ public class World {
 	 * @param numPrey number of prey
 	 * @param numPredator number of predators
 	 */
-	public World(int w, int h, int numPrey, int numPredator) {
+	public World(int w, int h, int numPrey, int numPredator)
+	{
 		width = w;
 		height = h;
-		canavasColor = Helper.newRandColor();
+		canvasColor = Helper.newRandColor();
 		
-		// Add Prey and Predators to the world.
+		// Add prey and predators
 		populate(numPrey, numPredator);
+	}
+
+	Color getCanvasColor()
+	{
+		return canvasColor;
 	}
 
 
 	/**
 	 * Generates numPrey random prey and numPredator random predators 
 	 * distributed throughout the world.
-	 * Prey must not be placed outside canavas!
+	 * Prey must not be placed outside canvas!
 	 *
 	 * @param numPrey the number of prey to generate
 	 * @param numPredator the number of predators to generate
 	 */
-	private void populate(int numPrey, int numPredators)
+	private void populate(int numPrey, int numPredator)
 	{
 		// Generates numPrey prey and numPredator predators 
 		// randomly placed around the world.
-		
+
 		//add predators
 		for(int i=0; i<numPredator; i++)
 		{
+			//add predator with given width and height
 			predators.add(new Predator(width, height));
 		}
 		//add prey
-		/*
 		for(int i = 0; i<numPrey; i++)
 		{
-			prey.add(new Prey(width, height, this));
+			//add prey with width, height, world (this), and a random color
+			prey.add(new Prey(width, height, this, Helper.newRandColor()));
 		}
-		*/
-		
+
 	}
 	
 	/**
 	 * Updates the state of the world for a time step.
 	 */
-	public void update() 
+	public void update()
 	{
 		// Move predators, prey, etc
-		
+
 		//create array lists for removing and adding predators and prey
 		ArrayList<Animal> removeList = new ArrayList();
 		ArrayList<Animal> addList = new ArrayList();
-		
+
 		//predators can reproduce, die, and move
 		for(Predator p: predators)
 		{
+			//check if the predator died naturally
+			if(p.dieNaturally())
+			{
+				removeList.add(p);
+			}
 			if(p.reproduce())
 			{
-				//create up to 3 offspring
+				//predators create up to 3 offspring
 				int numReproduced = Helper.nextInt(2)+1;
 				//add the predators given the random generated number of offspring
 				for(int i = 0; i<=numReproduced; i++)
@@ -85,17 +96,45 @@ public class World {
 					addList.add(new Predator(width, height));
 				}
 			}
-			//check if the predator is alive
-			if(p.checkAlive())
-			{
-				removeList.add(p);
-			}
-			//move the predator
+			//otherwise, move the predator
 			else
 			{
 				p.move();
 			}
 		}
+
+		for(Prey p: prey)
+		{
+			//if the prey is eaten, add it to the remove array list
+			if(p.getEaten())
+			{
+				removeList.add(p);
+			}
+			if (p.dieNaturally())
+			{
+				removeList.add(p);
+			}
+			if(p.reproduce())
+			{
+				//determine if the prey mutates
+				if(Helper.nextInt(99) <= p.getMutationChance())
+				{
+					//if the prey mutates, add a new prey with a different color
+					addList.add(new Prey(width, height, this, Helper.newRandColor()));
+				}
+				//if the prey does not mutate, give the new prey the same color
+				else
+				{
+					addList.add(new Prey(width, height, p.getAnimalX(), p.getAnimalY(), this));
+				}
+			}
+			//otherwise, move
+			else
+			{
+				p.move();
+			}
+		}
+
 		//remove a specific type of animal
 		for(Animal a: removeList)
 		{
@@ -105,17 +144,15 @@ public class World {
 				predators.remove(a);
 			}
 			//if the animal is a prey, remove the prey
-			/*else if(a instanceof Prey)
+			if(a instanceof Prey)
 			{
 				prey.remove(a);
 			}
-			
 			//if the animal is not one of these options, there is an error
 			else
 			{
 				System.out.println("Error in the code");
 			}
-			*/
 		}
 
 		//add a specific type of animal
@@ -127,7 +164,7 @@ public class World {
 				predators.add((Predator) a);
 			}
 			//otherwise, add prey
-			/*else if (a instanceof Prey)
+			if (a instanceof Prey)
 			{
 				prey.add((Prey) a);
 			}
@@ -136,28 +173,67 @@ public class World {
 			{
 				System.out.println("Error with code");
 			}
-			*/
 		}
+
 
 	}
 
 	/**
 	 * Draw all the predators and prey.
 	 */
-	public void draw()
+	void draw()
 	{
 		/* Clear the screen */
-		PPSim.dp.clear(canavasColor);
-		
-		// Draw predators and pray
-		
+		PPSim.dp.clear(canvasColor);
+
 		// Draw predators
 		for(Predator p: predators)
 		{
 			PPSim.dp.drawSquare(p.getAnimalX(), p.getAnimalY(),p.getColor());
 		}
+		//draw prey
+		for(Prey p: prey)
+		{
+			PPSim.dp.drawCircle(p.getAnimalX(), p.getAnimalY(), p.getColor());
+		}
 	}
 
+	//change the background
+	public void changeBackground()
+	{
+		//assign a random color
+		canvasColor = Helper.newRandColor();
+		//recheck if the prey's color matches the new background
+		for(Prey p: prey)
+		{
+			p.checkIfColorIsSame(canvasColor);
+		}
 
+	}
+
+	//add prey to the world for the mouse command
+	void addPrey(int animalX, int animalY)
+	{
+		prey.add(new Prey(width, height, animalX, animalY, this));
+	}
+
+	//reset simulation
+	void resetSimulation()
+	{
+		//create new array lists
+		predators = new ArrayList();
+		prey = new ArrayList();
+
+		//populate the initial simulation with 10 prey and 5 predators
+		populate(10,5);
+	}
+	//alternative simulation reset with less predators
+	void resetSimulationAlternate()
+	{
+		predators = new ArrayList();
+		prey = new ArrayList();
+
+		populate(15, 2);
+	}
 
 }
