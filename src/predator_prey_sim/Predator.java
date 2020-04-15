845e6c65d36071d99bc485178c24112cd90540a0
Kayleigh Calder <59836279+calderkayleigh@users.noreply.github.com>
2020-04-14 22:22:48 -0400

    Update Predator.java

--- src/predator_prey_sim/Predator.java
+++ src/predator_prey_sim/Predator.java
@@ -1,13 +1,11 @@
 package predator_prey_sim;
 
-import util.Helper;
-
 import java.awt.*;
-import java.util.ArrayList;
 
 public class Predator extends Animal
 {
-    ArrayList<Predator> predators = new ArrayList();
+    int eatenDistance = 1;
+    int moveDistance = 1;
 
     public Predator (int width, int height)
     {
@@ -16,64 +14,88 @@ public class Predator extends Animal
         //list out the functions of predators
         color = Color.RED;
         turnRate = 5;
-        deathRate = 5;
+        deathRate = 1;
         reproductionRate = 1;
         visualDistance = 15;
     }
-    //check to see if the predator is alive
-    boolean checkAlive()
-    {
-        return super.checkAlive();
-    }
-
-    //check to see if the predator will reproduce
-    boolean reproduce()
-    {
-        return super.reproduce();
-    }
-
-    public void changeDirection()
-    {
-        //determine if the predator will change direction
-        if(Helper.nextInt(99)> turnRate)
-        {
-            //change direction
-            super.changeDirection();
-        }
-
-    }
 
     //update predator position
-    //directions are up, left, down, and right
-    boolean move()
+    public void move()
     {
-        for(Predator p: predators)
+        //System.out.println("entering predator move class");
+        //need to define a boolean to determine if a predator has already been moved
+        boolean alreadyMoved = false;
+        for(Prey p: World.prey)
         {
-            if(direction == 0 && animalY + 1 < boundaryY)
+            //eat prey if it does not blend into the canvas
+            if(!p.checkIfColorIsSame(World.canvasColor))
             {
-                animalY +=1;
-                return true;
+                //System.out.println("entering predator move class and colors are different");
+                //if the distance between a predator and prey is less than 1, eat prey
+                if(Math.abs(animalX - p.animalX) <= eatenDistance || Math.abs(animalY - p.animalY) <= eatenDistance)
+                {
+                   // System.out.println("Prey was eaten");
+                    //eat prey
+                    p.eaten();
+                }
             }
-            else if(direction == 1 && animalX -1 > boundaryX)
+            //if the predator is not busy eating prey, check if there is a prey in sight
+            if(!alreadyMoved && !p.getEaten())
             {
-                animalX -=1;
-                return true;
+                alreadyMoved = checkDistanceBetweenPredatorsAndPrey(p);
             }
-            else if(direction == 2 && animalY -1 > boundaryY)
-            {
-                    animalY -=1;
-                    return true;
-
-            }
-            else if(direction ==3 && animalX +1 < boundaryX)
-            {
-                    animalX+=1;
-                    return true;
-            }
-            System.out.println("The predator did not move");
-            return false;
         }
+        if(alreadyMoved){return;}
+        else
+        {
+            //System.out.println("Predator moves normally");
+            //move the predator normally
+            super.move();
+        }
+    }
 
+    public boolean checkDistanceBetweenPredatorsAndPrey(Prey p)
+    {
+        //check the distance if the prey does not blend into the canvas
+        if(!p.checkIfColorIsSame(World.canvasColor))
+        {
+            if(direction == 0)
+            {
+                if(animalY - p.animalY <= visualDistance && animalY - moveDistance >1 && animalY - p.animalY > eatenDistance)
+                {
+                    //System.out.println("Predator moves towards prey north");
+                    animalY -=moveDistance;
+                    return true;
+                }
+            }
+            else if(direction == 1)
+            {
+                if(p.animalX - animalX <= visualDistance && animalX + moveDistance < boundaryX && p.animalX - animalX > eatenDistance)
+                {
+                    //System.out.println("Predator moves towards prey west");
+                    animalX +=moveDistance;
+                    return true;
+                }
+            }
+            else if(direction == 2)
+            {
+                if(p.animalY - animalY <= visualDistance && animalY + moveDistance < boundaryY && p.animalY - animalY > eatenDistance)
+                {
+                    //System.out.println("Predator moves towards prey south");
+                    animalY +=moveDistance;
+                    return true;
+                }
+            }
+            else if(direction == 3)
+            {
+                if(animalX - p.animalX <= visualDistance && animalX - moveDistance > 1 && animalX - p.animalX > eatenDistance)
+                {
+                    //System.out.println("Predator moves towards prey east");
+                    animalX -=moveDistance;
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
